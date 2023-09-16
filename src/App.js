4d2e8f39f1eb2b8fa1a5849222eab5853bee7086
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-09-16 09:25:59 +0200

    Feat: Create Home page dir + install MUI packages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
+import Home from "./pages/home/Home";
+import { Person } from "@mui/icons-material";
 
 function App() {
-  return (
-    <div>
-      Connect
-    </div>
-  );
+  return <Person />;
 }
 
 export default App;
